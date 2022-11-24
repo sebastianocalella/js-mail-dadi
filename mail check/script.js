99079c24c51c32d05e0fad4d8396debfad4823a0
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-24 15:09:36 +0100

    introduced mail and html elements

---
+++ mail check/script.js
@@ -0,0 +1,30 @@
+const buttonElement = document.getElementById('check');
+const mailElement = document.getElementById('usermail');
+const mailList = 
+[
+'pincopallo-1@gmail.com',
+'pincopallo-2@gmail.com',
+'pincopallo-3@gmail.com',
+'pincopallo-4@gmail.com',
+'pincopallo-5@gmail.com',
+'pincopallo-6@gmail.com',
+'pincopallo-7@gmail.com',
+'pincopallo-8@gmail.com',
+'pincopallo-9@gmail.com',
+'pincopallo-10@gmail.com',
+'pincopallo-11@gmail.com',
+'pincopallo-12@gmail.com',
+'pincopallo-13@gmail.com',
+'pincopallo-14@gmail.com',
+'pincopallo-15@gmail.com',
+'pincopallo-16@gmail.com',
+'pincopallo-17@gmail.com',
+'pincopallo-18@gmail.com',
+'pincopallo-19@gmail.com',
+'pincopallo-20@gmail.com',
+'pincopallo-21@gmail.com',
+'pincopallo-22@gmail.com'
+];
+
+console.log(mailList);
+console.log(usermail.value);
