01af4ee9c67df62846a53f80f8ab08117a3e7470
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-24 14:44:37 +0100

    game logic structure

--- Gioco dei dadi/script.js
+++ Gioco dei dadi/script.js
@@ -7,5 +7,27 @@ const option = [1,2,3,4,5,6];
 let userNumber = option[Math.floor(Math.random()*option.length)];
 console.log(userNumber);
 
-let pcNumber= option[Math.floor(Math.random()*option.length)];
+let pcNumber = option[Math.floor(Math.random()*option.length)];
 console.log(pcNumber);
+
+// dichiaro una variabile che identifica il bottone di gioco
+
+const playButton = document.getElementById('play');
+
+//Creo un comando che rimandi a schermo l'esito del gioco quando l'utente preme il bottone
+
+    //dichiaro una variabile che identifica il <p> relativo alla risposta
+
+const answer = document.getElementById('answer');
+
+playButton.addEventListener('click', function(){
+    if (userNumber > pcNumber) {
+        // '<p> dannazione hai vinto!</p>'
+    } else if (userNumber < pcNumber){
+        // '<p> Ahah! ho vinto io!</p>'
+    } else {
+        // '<p> Uno stallo alla messicana, vale la pena giocare di nuovo!</p>'
+    }
+})
+
+
