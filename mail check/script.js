b76178da6e689b342342d0a29b13ceba2399784b
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-25 10:05:04 +0100

    code cleaning

--- mail check/script.js
+++ mail check/script.js
@@ -28,17 +28,15 @@ const mailList =
 console.log(mailList);
 
 let answerElement = document.getElementById('answer');
-let answer = answerElement.value = 'Siamo spiacenti ma la mail inserita non risulta presente nella nostra mail list';
 
 buttonElement.addEventListener('click',function(){
     const mailElement = document.getElementById('usermail');
+    let answer = answerElement.value = 'Siamo spiacenti ma la mail inserita non risulta presente nella nostra mail list';
     for (let i=0; i<mailList.length; i++){
         if (mailElement.value == mailList[i]){
             answer = `ciao ${mailList[i]} ti confermiamo la tua presenza all'interno della nostra mail list!`;
         } else {}
     }
     answerElement.innerHTML = answer;
-
-    answer = 'Siamo spiacenti ma la mail inserita non risulta presente nella nostra mail list';
-})
+});
 
